0ab1134ea9a697d0b6e78c7d205f2d53fe1326b8
mark <mark.lepnjov@khk.ee>
2020-12-15 14:00:50 +0200

    xd

---
+++ Jareltoo.py
@@ -0,0 +1,19 @@
+import random # suvalise numbri generaator
+basseiniotsad = [] # tühja nimekirja tekitamine
+for i in range(5): # 5 erinevat basseiniotsa kogust
+    basseiniotsad.append(random.randint(30,50)) # lisab nimekirja
+
+meetrid = int(input("Sisestage, mitu meetrit on üks basseiniots: ")) #input kasutaja jaoks
+
+def kalorid(ujutud, pikkus): # funktsiooni defineerimine
+    kulunud = (ujutud*pikkus/1000)*120
+    return kulunud
+
+
+kokku = 9 # muutuja egitamine
+for y in range(len(basseiniotsad)):  # basseiniotste pikkusega tsükkel
+    kaloreid = kalorid(basseiniotsad[y],meetrid)
+    print("Kaloreid kulus: " + str(kaloreid))
+    kokku = kokku + kaloreid
+
+print("Kokku kulus " + str(round(kokku)) + " kalorit.")
